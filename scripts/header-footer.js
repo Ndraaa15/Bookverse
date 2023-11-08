79807521b6b216aa8e730741facd6a3f7f33ce76
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-11-08 18:19:22 +0700

    `Update`: index.html

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -1,8 +1,11 @@
 const toggleButton = document.querySelector(".toggle-button");
 const toggleButtonIcon = document.querySelector(".toggle-button i");
-const dropDownMenu = document.querySelector(".dropdown_menu");
+const dropDownMenu = document.querySelector(".dropdown-menu");
 
 toggleButton.onclick = function () {
-  toggleButtonIcon.classList.toggle("fa-times");
-  dropDownMenu.classList.toggle("open");
-};
+  dropDownMenu.classList.toggle('open')
+  const isOpen  = dropDownMenu.classList.contains('open')
+
+  toggleButtonIcon.classList = isOpen ? 'fa-solid fa-times' : 'fa-solid fa-bars'
+
+}
