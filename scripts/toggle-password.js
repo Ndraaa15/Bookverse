22d8cd423f5b248fea77ab08d6cb2bf803d383e8
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-11-14 15:30:57 +0700

    [indra] : add toggle password in signup page

---
+++ scripts/toggle-password.js
@@ -0,0 +1,25 @@
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById('password');
+  var toggleIcon = document.querySelector('.toggle-password');
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    toggleIcon.src = './assets/open-password.png'; // Change to the path of your closed-eye icon
+  } else {
+    passwordInput.type = 'password';
+    toggleIcon.src = './assets/closed-password.png'; // Change to the path of your open-eye icon
+  }
+}
+
+function toggleConfirmPasswordVisibility() {
+  var passwordInput = document.getElementById('confirm-password');
+  var toggleIcon = document.querySelector('.toggle-confirm-password');
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    toggleIcon.src = './assets/open-password.png'; // Change to the path of your closed-eye icon
+  } else {
+    passwordInput.type = 'password';
+    toggleIcon.src = './assets/closed-password.png'; // Change to the path of your open-eye icon
+  }
+}
